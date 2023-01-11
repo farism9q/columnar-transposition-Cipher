1000b6ab5f440fec466c1f0e681e9c1dd254638a
farism9q <faresm9q@gamil.com>
2023-01-12 02:54:22 +0300

    creating decryption fucntion

--- src/ColmnuarTransposiotionCipher.java
+++ src/ColmnuarTransposiotionCipher.java
@@ -3,18 +3,21 @@ import java.util.Arrays;
 public class ColmnuarTransposiotionCipher {
 	public String plainText;
 	public String key;
+	public String cipherText;
 	
 	public ColmnuarTransposiotionCipher() {
 		this.plainText = "";
 		this.key = "";
+		this.cipherText = "";
 	}
 	
 	public String encrypt() {
 //		Math.ceil() accepts double, we did casting to round it to upper number and we did another casting to convert it to char type
 		int nbColumns = key.length(); 
-		char[][] table = new char [(char)Math.ceil( (double) plainText.length()/nbColumns)][nbColumns];
+    double nbRows = Math.ceil((double) plainText.length()/nbColumns);
+		char[][] table = new char [(char)nbRows][nbColumns];
 		int plainIdx = 0;
-		
+		String keyCopy = key; 
 		 
 //		filling the table:
 		for (int i = 0; plainIdx < plainText.length(); i++) { // fill the array until the plain text finishes
@@ -24,26 +27,22 @@ public class ColmnuarTransposiotionCipher {
 		}
 		
 //		Sorting the key:
-		char a [] = key.toCharArray();
+		char a [] = keyCopy.toCharArray();
 		 Arrays.sort(a);
 		
 //		plaint text length - key length = nb of rows 
 //		picking up the cipher text:
-		String cipherText = "";
+//		String cipherText = "";
 		int sortedIdx = 0;
 		
-		for (int i = key.indexOf(a[sortedIdx]); sortedIdx < key.length();) {
-			i = key.indexOf(a[sortedIdx++]);				
+		for (int i = keyCopy.indexOf(a[sortedIdx]); sortedIdx < keyCopy.length();) {
+			i = keyCopy.indexOf(a[sortedIdx++]);				
 			
 				
-			for (int j = 0; j < Math.ceil( (double) plainText.length()/nbColumns) && table[j][i] != 0  && cipherText.length() != plainText.length(); j++) { //5 - 3 = 2
+			for (int j = 0; j < nbRows && table[j][i] != 0  && cipherText.length() != plainText.length(); j++) { //5 - 3 = 2
 				cipherText+=table[j][i]; 
 			} 
-			key = key.replaceFirst(key.charAt(i)+"", "*"); // to get rid of first char and find next occurrence 
-			
-			
-			
-
+			keyCopy = keyCopy.replaceFirst(keyCopy.charAt(i)+"", "*"); // to get rid of first char and find next occurrence 
 		}
 		
 //		System.out.print("Your cipher text is: ");
@@ -51,6 +50,48 @@ public class ColmnuarTransposiotionCipher {
 		return cipherText;
 
 	}
+	
+	public String decrypt() {
+    int nbColumns = key.length(); 
+    double nbRows = Math.ceil((double) cipherText.length()/nbColumns);
+		char[][] table = new char [(char)nbRows][nbColumns];
+		int cipherIdx = 0;
+		String keyCopy = key; 
+		int nbLetterOflastRow = cipherText.length() % nbColumns;
+
+    // // filling the table:
+    // for (int i = 0; cipherIdx < plainText.length(); i++) { // fill the array until the plain text finishes
+		// 	for (int j = 0; j < nbColumns && cipherIdx < plainText.length(); j++) { // fill in a row way + stop when the plain text finished 
+		// 		table[i][j] = cipherText.charAt(cipherIdx++);
+		// 		}
+		// }
+
+    char a [] = keyCopy.toCharArray();
+		 Arrays.sort(a);
+		
+//		plaint text length - key length = nb of rows 
+//		picking up the cipher text:
+		String decryptedText = "";
+		int sortedIdx = 0;
+		for (int i = keyCopy.indexOf(a[sortedIdx]); sortedIdx < keyCopy.length();) {
+			i = keyCopy.indexOf(a[sortedIdx++]);				
+			
+				
+			for (int j = 0; j < nbRows && decryptedText.length() != cipherText.length(); j++) { //5 - 3 = 2
+				if(j != nbRows-1 || i+1 <= nbLetterOflastRow || nbLetterOflastRow == 0) //fill up only if you are not in the last row OR not in the column that > nbLetterOflastRow 
+				table[j][i]+=cipherText.charAt(cipherIdx++); 
+			} 
+			keyCopy = keyCopy.replaceFirst(keyCopy.charAt(i)+"", "*"); // to get rid of first char and find next occurrence 
+		}
+
+		cipherIdx = 0;
+    for (int i = 0; cipherIdx < cipherText.length(); i++) { // fill the array until the plain text finishes
+			for (int j = 0; j < nbColumns && cipherIdx++ < cipherText.length(); j++) { // fill in a row way + stop when the plain text finished 
+				decryptedText+=table[i][j];
+				}
+		}
+		return decryptedText;
+		}
 	
 	
 	
--- src/main.java
+++ src/main.java
@@ -21,14 +21,24 @@ public class main {
 		ColmnuarTransposiotionCipher c1 = new ColmnuarTransposiotionCipher();
 		
 		Scanner scan = new Scanner(System.in);
-		
+//		
 		System.out.print("Enter plain text to be encrypted: ");
 		c1.plainText = scan.next();
 
 		System.out.print("Enter your key: ");
 		c1.key = scan.next();
 		
-		System.out.println(c1.encrypt());
+		System.out.println("Your cipher text: " + c1.encrypt());
+//		
+		
+		System.out.print("Enter cipher text to be deprycted: ");
+		c1.cipherText = scan.next();
+
+		System.out.print("Enter your key: ");
+		c1.key = scan.next();
+		
+		System.out.println("Your decrypted text: " + c1.decrypt());
+		
 
 		
 		
